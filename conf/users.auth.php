bcc74152d6cc1e31db281c06f2918518aa105f52
oli-v-ier <oli_v_ier@yahoo.fr>
2011-02-08 00:22:04 -0400

    Creation fichiers conf

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+oli_v_ier:ce4efb4a91532613278dd55a7fae502a:Olivier:oli_v_ier@yahoo.fr:admin,user
